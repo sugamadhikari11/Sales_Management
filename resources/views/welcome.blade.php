11e705c46ddefbe4704c3337a394a17565f351d7
Sugam Adhikari <sugam.19217113@gmail.com>
2024-07-12 23:39:47 +0545

    bootstrap added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,15 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="home">
-        <div class="text">Home Page</div>
-    </section>
+<section class="home py-5">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8 text-center">
+                <h1 class="display-4 mb-4">Welcome to the Home Page</h1>
+                <p class="lead">This is the home page of your Laravel application. Use this space to provide some information or a brief introduction.</p>
+                <a href="{{ url('/settings') }}" class="btn btn-primary btn-lg mt-3">Go to Settings</a>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
