db2b700d046c74b7d5a9734fe911cc2687617c77
Sugam Adhikari <sugam.19217113@gmail.com>
2024-07-13 23:53:45 +0545

    made small change in settings page

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -4,5 +4,13 @@
 @section('content')
     <section class="home">
         <div class="text">Settings Page</div>
+
+        <div class="options">
+            <h2>Change Password</h2>
+            <!-- Add your change password form or link here -->
+
+            <h2>Edit Profile</h2>
+            <!-- Add your edit profile form or link here -->
+        </div>
     </section>
 @endsection
